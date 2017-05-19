debd58a48c79ab8627214b8d3722f2901f1feefa
dorian smiley <doriansmiley@siliconpublishing.com>
2017-05-18 18:05:06 -0700

    add latest core code

--- src/util/BrowserUtils.ts
+++ src/util/BrowserUtils.ts
@@ -6,7 +6,7 @@ export class BrowserInfo {
     public browser:string;
     public version:string;
 
-    constructor(browser, version) {
+    constructor(browser?:string, version?:string) {
         this.browser = browser;
         this.version = version;
     }
@@ -15,21 +15,39 @@ export class BrowserInfo {
 export class BrowserUtils {
     static uaMatch(ua):BrowserInfo {
         ua = ua.toLowerCase();
-        let match = /(ipad).*(?:safari)[ \/]([\w.]+)/.exec(ua) ||
-            /(chrome)[ \/]([\w.]+)/.exec(ua) ||
-            /(safari)[ \/]([\w.]+)/.exec(ua) ||
-            /(firefox)[ \/]([\w.]+)/.exec(ua) ||
-            /(webkit)[ \/]([\w.]+)/.exec(ua) ||
-            /(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
-            /(msie) ([\w.]+)/.exec(ua) ||
-            ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) ||
+        var match = /(ipad).*(?:safari)[ \/]([\w.]+)/.exec( ua ) ||
+            /(android)[ chrome\/]([\w.]+)/.exec( ua ) ||//MUST BE BEFORE CHROME
+            /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+            /(safari)[ \/]([\w.]+)/.exec( ua ) ||
+            /(firefox)[ \/]([\w.]+)/.exec( ua ) ||
+            /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+            /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+            /(msie) ([\w.]+)/.exec( ua ) ||
+            /trident.*rv[ :]*[1-9]\d\./.exec( ua.toLowerCase() ) ||
+            ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
             [];
+
+        //IE 11+ changed user agents, AWESOME!!! More info here: http://stackoverflow.com/questions/17447373/how-can-i-target-only-internet-explorer-11-with-javascript
+        //We need to reset to the expected id forIE
+        if( /trident.*rv[ :]*[1-9]\d\./.exec( ua.toLowerCase() ) ){
+            match[ 1 ] = 'msie';//reset to msie
+            match[ 2 ] = /[1-9]\d\./.exec( ua );
+        }
+
         let browser = match[1] || "";
         let version = match[2] || "0";
         return new BrowserInfo(browser, version);
     }
 
     static getBrowser():BrowserInfo {
-        return BrowserUtils.uaMatch(navigator.userAgent);
+        var returnValue:BrowserInfo = new BrowserInfo('nodeJS', 'nodeJS')
+        try{
+            if(navigator){
+                returnValue = BrowserUtils.uaMatch(navigator.userAgent);
+            }
+        }catch(e){
+            console.log(e.stack);
+        }
+        return returnValue;
     }
 }
--- util/BrowserUtils.js
+++ util/BrowserUtils.js
@@ -4,14 +4,22 @@ Lavender.BrowserUtils = function(){
 Lavender.BrowserUtils.uaMatch = function( ua ) {
     ua = ua.toLowerCase();
     var match = /(ipad).*(?:safari)[ \/]([\w.]+)/.exec( ua ) ||
-                /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
-                /(safari)[ \/]([\w.]+)/.exec( ua ) ||
-                /(firefox)[ \/]([\w.]+)/.exec( ua ) ||
-                /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
-                /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
-                /(msie) ([\w.]+)/.exec( ua ) ||
-                ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
-            [];
+        /(android)[ chrome\/]([\w.]+)/.exec( ua ) ||//MUST BE BEFORE CHROME
+        /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+        /(safari)[ \/]([\w.]+)/.exec( ua ) ||
+        /(firefox)[ \/]([\w.]+)/.exec( ua ) ||
+        /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+        /(msie) ([\w.]+)/.exec( ua ) ||
+        /trident.*rv[ :]*[1-9]\d\./.exec( ua.toLowerCase() ) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+        [];
+    //IE 11+ changed user agents, AWESOME!!! More info here: http://stackoverflow.com/questions/17447373/how-can-i-target-only-internet-explorer-11-with-javascript
+    //We need to reset to the expected id forIE
+    if( /trident.*rv[ :]*[1-9]\d\./.exec( ua.toLowerCase() ) ){
+        match[ 1 ] = 'msie';//reset to msie
+        match[ 2 ] = /[1-9]\d\./.exec( ua );
+    }
 
     return {
             browser: match[ 1 ] || "",
