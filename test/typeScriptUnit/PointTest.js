b78c5b90c217a9a581f1d4933a67e94132676095
dorian smiley <doriansmiley@yahoo.com>
2017-07-11 13:44:40 -0700

    update test

--- test/typeScriptUnit/PointTest.js
+++ test/typeScriptUnit/PointTest.js
@@ -4,28 +4,28 @@
 describe('Lavender.Point', function(){
 
     it('Testing Lavender.Point', function() {
-        var point = Lavender.Point();
+        var point = new Lavender.Point();
         expect(point.x).toBe(0);
         expect(point.y).toBe(0);
         point.x = 5;
         point.y = 15;
         expect(point.x).toBe(5);
         expect(point.y).toBe(15);
-        var point2 = Lavender.Point(20,25);
+        var point2 = new Lavender.Point(20,25);
         expect(point2.x).toBe(20);
         expect(point2.y).toBe(25);
-        console.log(Lavender.Point.distance(point, point2));
-        console.log(Lavender.Point.direction(point, point2));
-        expect(point2.add(point)).toBe(false);
-        expect(point2.x).toBe(10);
-        expect(point2.y).toBe(30);
-        expect(point2.subtract(point)).toBe(false);
-        expect(point2.x).toBe(5);
-        expect(point2.y).toBe(15);
-        expect(point2.scale(10)).toBe(false);
-        expect(point2.x).toBe(50);
-        expect(point2.y).toBe(150);
-        console.log(point2.magnitude());
+        expect(Lavender.Point.distance(point, point2)).toBe(18.027756377319946);
+        expect(Lavender.Point.direction(point, point2)).toBe(0.5880026035475675);
+        point2 = point2.add(point)
+        expect(point2.x).toBe(25);
+        expect(point2.y).toBe(40);
+        point2 = point2.subtract(point)
+        expect(point2.x).toBe(20);
+        expect(point2.y).toBe(25);
+        point2 = point2.scale(10)
+        expect(point2.x).toBe(200);
+        expect(point2.y).toBe(250);
+        expect(point2.magnitude()).toBe(320.1562118716424);
     });
 
 });
